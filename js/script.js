fd88c4bd17a83affacb88da12883040852be9302
doniyorjonjaloliddinov <don2003.01.09@gmail.com>
2022-03-07 14:34:17 +0500

    add all lab_works from 1 to 6

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function reverse_a_number(n)
+{
+  n = n + "";
+  return n.split("").reverse().join("");
+}
+alert(reverse_a_number(987654321));
+
+
+let elementHead = document.querySelector("head");
+console.log(elementHead);
+
+
+let listName = querySelectorAll("li");
